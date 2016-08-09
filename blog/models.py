e81e2d22828f32b5f059233c8c0ab0a327515225
will <william@nanoprecisionmedical.com>
2016-08-09 08:06:04 -0700

    added pages, class views

--- blog/models.py
+++ blog/models.py
@@ -7,7 +7,7 @@ from django.core.urlresolvers import reverse
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
-        return super(PublishedManager, self).get_queryset().filter(staus='published')
+        return super(PublishedManager, self).get_queryset().filter(status='published')
 
 
 class Post(models.Model):
